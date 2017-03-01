a11d54b832fec6cd5b6caf2835088f18efe38e93
Kyle Eisenbarger <keisenb@ksu.edu>
2017-02-28 23:06:35 -0600

    Working youtube plugin

---
+++ resources/views/layouts/youtube.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no" />
+  <meta name="description" content="The Kansas State University Computer Science Atrium Display for Spring 2017">
+  <meta name="author" content="Kyle Eisenbarger">
+  <title>K-State Atrium Display | @yield('title')</title>
+
+  @yield('head')
+</head>
+
+<body>
+
+
+@yield('content')
+
+@yield('footer')
+</body>
+
+</html>
--- resources/views/youtube.blade.php
+++ resources/views/youtube.blade.php
@@ -1,21 +1,43 @@
+@extends('layouts.youtube')
 @section('title', 'Youtube')
 
 @section('head')
-<link href="{{ URL::asset('css/jquery.mb.YTPlayer.css') }}" media="all" rel="stylesheet" type="text/css">
+<link href="{{ URL::asset('css/jquery.mb.YTPlayer.min.css') }}" media="all" rel="stylesheet" type="text/css">
+<link href="{{ URL::asset('css/youtube.css') }}" media="all" rel="stylesheet" type="text/css">
+
 @endsection
 
 @section('content')
 
-<div id="bgndVideo" class="player" data-property="{videoURL:'https://youtu.be/J4peLBZ6ZCI',containment:'header',autoPlay:true, loop:true, mute:true, showControls:false, quality:'hd1080'}"></div>
+
+<body>
+<div id="wrapper">
+  <div id="customElement">
+      <div id="testText">
+          <h1>Kansas State University</h1>
+          <h2>Computer Science Department
+      </div>
+  </div>
+</div>
+
+<div id="video" class="player" data-property="{videoURL:'https://youtu.be/J4peLBZ6ZCI',containment:'#customElement', showControls:false, autoPlay:true, loop:true, mute:true, startAt:0, opacity:1, addRaster:true, quality:'hd1080'}"></div> <!--BsekcY04xvQ-->
+
+
 
 
 @endsection
 
 @section('footer')
-<script src="{{ URL::asset('js/jquery.mb.YTPlayer.src.js') }}"></script>
+<script src="{{ URL::asset('js/jquery.min.js') }}"></script>
+<script src="{{ URL::asset('js/jquery.mb.YTPlayer.min.js') }}"></script>
 <script>
-jQuery(function(){
-  jQuery("#bgndVideo").YTPlayer();
-});
+    var myPlayer;
+    jQuery(function () {
+
+        myPlayer = jQuery(".player").YTPlayer();
+    });
 </script>
+
+
+
 @endsection
